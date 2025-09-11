045c9b7638c1082a245673fbc288a33c14decbf7
Olusegun Oyagbesan <105999722+Festusolusegun@users.noreply.github.com>
2025-09-11 08:53:51 +0100

    Update urls.py

--- survey/urls.py
+++ survey/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.questionnaire_view, name="questionnaire"),
     path('thank-you/', views.thank_you_view, name="thank_you"),
     path('results/', views.results_view, name="results"),
+    path('export-csv/', views.export_responses_csv, name='export_responses_csv'),
 ]
 
