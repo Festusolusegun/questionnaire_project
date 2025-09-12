02b9e1359a660d478aa193f532ba2ef21b9f4f2a
Olusegun Oyagbesan <105999722+Festusolusegun@users.noreply.github.com>
2025-09-12 21:29:43 +0100

    Update urls.py

--- survey/urls.py
+++ survey/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
@@ -6,5 +7,10 @@ urlpatterns = [
     path('thank-you/', views.thank_you_view, name="thank_you"),
     path('results/', views.results_view, name="results"),
     path('export-csv/', views.export_responses_csv, name='export_responses_csv'),
+
+    # login/logout
+    path('login/', auth_views.LoginView.as_view(template_name='survey/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+
 
