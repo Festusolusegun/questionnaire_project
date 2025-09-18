8cf450531aac6a9bbfac61d55f015ea889659efd
Olusegun Oyagbesan <105999722+Festusolusegun@users.noreply.github.com>
2025-09-18 21:16:54 +0100

    Update urls.py

--- questionnaire_project/urls.py
+++ questionnaire_project/urls.py
@@ -1,16 +1,17 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views   # 👈 import Django’s auth views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('survey.urls')),   # your survey app routes
-
-    # 🔑 Authentication routes
-# Authentication routes (use default template location: registration/login.html)
-    path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(next_page="/"), name="logout"),
+    path('', include('survey.urls')),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(next_page="/"), name='logout'),
 ]
+
+if settings.DEBUG:  # serve uploads in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
 
