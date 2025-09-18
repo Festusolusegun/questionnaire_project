8e08d4240182f1cb9a455a88ad83efc07e954d79
Olusegun Oyagbesan <105999722+Festusolusegun@users.noreply.github.com>
2025-09-18 21:40:19 +0100

    Update form.py

--- survey/form.py
+++ survey/form.py
@@ -6,10 +6,24 @@ WOMAC_CHOICES = [(i, str(i)) for i in range(5)]  # 0–4 scale
 class QuestionnaireForm(forms.ModelForm):
     class Meta:
         model = QuestionnaireResponse
-        fields = [
-            "name", "age", "sex", "phone", 
+           fields = [
+            "name", "age", "sex", "phone",
             "had_tkr", "tkr_date", "radiograph",
-             "WOMAC", "VAS", "satisfaction", "ambulation"]   # ✅ ensures demographics + WOMAC + VAS + Satisfaction + Ambulation all show up
+            "vas_pain",
+            "womac_pain_walking", "womac_pain_stairs", "womac_pain_nocturnal",
+            "womac_pain_rest", "womac_pain_weight_bearing",
+            "womac_stiffness_morning", "womac_stiffness_later_day",
+            "womac_function_descend_stairs", "womac_function_ascend_stairs",
+            "womac_function_rising_sitting", "womac_function_standing",
+            "womac_function_bending_floor", "womac_function_walking_flat",
+            "womac_function_in_out_car", "womac_function_shopping",
+            "womac_function_putting_socks", "womac_function_lying_bed",
+            "womac_function_taking_socks_off", "womac_function_rising_bed",
+            "womac_function_in_out_bath", "womac_function_sitting",
+            "womac_function_on_off_toilet", "womac_function_heavy_domestic",
+            "womac_function_light_domestic",
+            "satisfaction", "ambulation"
+        ]  # ✅ ensures demographics + WOMAC + VAS + Satisfaction + Ambulation all show up
         widgets = {
             # Demographics
             'name': forms.TextInput(attrs={'class': 'form-control'}),
